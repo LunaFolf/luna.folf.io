c51bf77158fc4fa12bfb5a700930ede26092c577
WhiskeeDev <email@liamholmes.co.uk>
2023-09-20 16:24:03 +0100

    Refine text labels and reformat content in App.tsx
    Redesigned the "About Me", "Experience", "Skills", and "Projects" sections of the portfolio paged to give them a more explanatory label and improve content readability. Import css files have been moved to 'index.css'. These changes present a more inviting user interface and attractive readability friendly chronological storyline of the professional journey.

--- src/App.tsx
+++ src/App.tsx
@@ -20,18 +20,18 @@ function App() {
       </div>
       <div>
         <hr/>
-        <div className={"text-title-1"}>About Me</div>
+        <div className={"text-title-1"}>Brief Introduction</div>
         <hr/>
         <div className={"space-me-please"}>
-          <p>I began learning basic programming skills such as HTML from very early years during my secondary school education, which I developed by studying Computer Science and IT courses during my GCSEs.</p>
-          <p>began examining AS Level Computer Science before leaving to progress via a development apprenticeship in 2017.</p>
-          <p>I’ve led multiple development projects, both backend and frontend, within companies, and I’m currently developing a CRM to manage my discord gaming community.</p>
-          <p>I currently spend most of my focus on Frontend development and UI/UX Design, and using NodeJS as a main alternative for backend applications instead of PHP and other languages.</p>
+          <p>I began learning basic programming skills before I was even in Secondary School. I then developed further by studying Computer Science and IT courses during my GCSEs.</p>
+          <p>I completed AS Level Computer Science, before leaving to progress via a development apprenticeship in 2017.</p>
+          <p>I’ve led multiple development projects, both backend and frontend, using key frameworks such as Vue.js, Laravel and a bit of React.</p>
+          <p>I'm currently focusing on Node.js backend development, as well as exploring UI/UX Design within my Frontend development.</p>
         </div>
       </div>
       <div>
         <hr/>
-        <div className={"text-title-1"}>Experience</div>
+        <div className={"text-title-1"}>My Work Experience</div>
         <hr/>
         <div className={"space-me-please"}>
           <div className={"text-headline"}>Fullstack Web Developer</div>
@@ -61,7 +61,7 @@ function App() {
       </div>
       <div>
         <hr/>
-        <div className={"text-title-1"}>Skills</div>
+        <div className={"text-title-1"}>My Skills</div>
         <hr/>
         <div className={"space-me-please"}>
           <div className={"text-title-2"}>~8-10 years experience in:</div>
@@ -97,13 +97,57 @@ function App() {
       </div>
       <div>
         <hr/>
-        <div className={"text-title-1"}>Projects</div>
+        <div className={"text-title-1"}>Products/Projects I've worked on</div>
         <hr/>
         <div className={"space-me-please"}>
+          <div className={"text-title"}>Mobilleo (FOD Mobility Group) 2019-2023</div>
+          <p>Mobilleo is a all-in-one "mobility-as-a-service" solution for businesses for urban, city-to-city and country-to-country travel.</p>
+          <p>During my 4 years working with Mobilleo, starting with a team of just 3 Frontend developers, I've upgraded frameworks, redesigned entire features, solo implemented new complex features, introduced improved payment flows and integrated 3D Secure.</p>
+          <p>In more recent time, I've convinced the business to allow us to rewrite the frontend, using React and MFE (Micro-Frontends) using Module Federation.</p>
+          <p>I've also unofficially been the Teams UI/UX designer for some of this time, creating designs in Sketch/Adobe XD/Figma to sell new features, and to assist in implementations of new features where higher ups had been blocked because they couldn't picture how the feature would work.</p>
+
+          <div className={"text-title"}>Mobilleo Payments (FOD Mobility Group) 2023</div>
+          <p>Due to Technical limitations, and an aggressive time-crunch, our mobile app development team requested web team's assistance with integrating payments into their apps.</p>
+          <p>The result was a External Payments web page developed solely by myself using TypeScript and no frameworks, and all custom CSS.</p>
+          <p>This external web page was designed/development in such a way, that any platform or product could theoretically use it to make payments for a Mobilleo Booking, and I designed it carefully to work well across Mobile and Desktop Screens.</p>
+
+          <div className={"text-title"}>Mobilleo PWA (FOD Mobility Group) 2020-2023</div>
+          <p>Mobilleo originally was purely for the Desktop, if you tried to open it on your phone it was essentially unusable.</p>
+          <p>Open it anytime between 2020 and 2023 however, and you'll find a carefully crafted UI designed almost as if it was a mobile app.</p>
+          <p>in 2020, maybe before then my dates are fuzzy, Mobilleo was developing (or re-developing) their Android app, and unfortunately it wasn't going to be finished in time for release.</p>
+          <p>Realising this, out of hours I began working on an update to allow Mobilleo to work on both Desktop and Mobile. It wasn't perfect, and it only supported customer facing screens, but it was enough to get us to pass our deadline while the official app was continued worked on.</p>
+          <p>In 2023, maintaining the Desktop and Mobile aspects of the app began slowing down development, and with new iOS and Android apps on the way the decision was made to remake Mobilleo in React and drop support for Mobile, focusing once again only on Desktop.</p>
+
+          <div className={"text-title"}>Maintenance Central (Europlus Direct) 2017-2019</div>
+          <p>Maintenance Central was an in-house CRM system, designed and developed for supporting customers of Lenovo, IBM, Hewlett Packard, and more, with their product warranties.</p>
+          <p>When I was hired, as a Junior Apprentice, Maintenance Central was Laravel for Frontend and Backend, however 7 months later we switched to Vue.js for the front-end, and I became the sole developer and maintainer for the entire product.</p>
+          <p>I took lead in several developments during my time with the company, one of which was the development of an "Operations Dashboard", again involving both front-end and back-end development. During this I worked closely with the  operations team members to get their ideas and feedback.</p>
+
+          <div className={"text-title"}>Data Processing Tool [DPT] (Europlus Direct) circa 2018</div>
+          <p>During my time working with Europlus, one of my tasks was a data import job. This involved extracting Gigabytes of customer warranty data from several excel sheets, all in different formats, from different vendors (Lenovo, IBM, etc).</p>
+          <p>This extracted data then needed to be formatted, checked against external warranty checking sites to see if it was out of date data (i.e. a customer had already renewed), and also needed to be sanitised and in some cases corrected.</p>
+          <p>This job could take several days, and when factoring in importing the data into the system this sometimes could take an entire week... an entire week of the sole developer, not doing any development...</p>
+          <p>In addition to this, I was also expected to create reports on the data each month. How many new customers? How many existing? How many were unprocessable? How many had to be fixed? How many already had been renewed?</p>
+          <p>This almost became my entire job, and was extremely stressful (I was a Junior Dev, not a data analyst!), luckily I came up with a solution - Data Processing Tool [DPT]</p>
+          <p>DPT was written in Electron, and communicated directly with the vendor's APIs, alternatively it also could use an in-house API that could check the information faster, keep up to date records and keep caches.</p>
+          <p>So how fast was DPT? In a worst case scenario, DPT could process 3,000 serial numbers in less than a minute.</p>
+          <p>But that wasn't all, because in addition to exporting clean usable data for imports, it would split the import files by weeks the products were due to expire, so we could import one week ahead at a time - having data ready for our sales team.</p>
+          <p>Oh and those reports I mentioned? Yeah, it auto-generated all them too.</p>
+
+          <div className={"text-title"}>Trestle (<a href={"https://trestle.js.org"}>https://trestle.js.org</a>)</div>
+          <p>Trestle is my own attempt at creating a Node.js REST API Package, solely based off my experience. Before creating Trestle, I had no experience with any other similar frameworks such as Express or Nest.js</p>
+          <p>I based it loosely off of Laravel/Lumen, and built it using my own personal preferences. For some reason, I was very keen on making it "hot-swappable" as well.</p>
+          <p>While the base v2.0.0 package features a basic TrestleAPI and route, as well as automatic OpenAPI spec generation, future versions planned to support Databases, Authentication, Middleware and Transformers (just like Laravel), and WebSockets</p>
+          <p>The project has been inactive for some time now, and needs a rewrite in TypeScript (it was originally written in just JavaScript). I also need to finish the spec Trestle uses, JSendPlus, which was my own modified fork of the JSend specification.</p>
+
           <div className={"text-title"}>JaxBot</div>
           <p>JaxBot was a CRM application, that also integrated with discord via their bot API. I custom built it for a gaming community I originally ran, and it had the ability to record users, issue warnings to users by staff, and manage forms/applications.</p>
           <p>It was created using Node.js, and used AWS RDS for database management/storage. Jax also allowed for our game servers to integrate with our discord. For example, in some games if a player was killed they would be muted in Discord until the next round.</p>
-          <p>Jax was eventually shutdown when ownership of the community was passed on, and the new owner had different plans for managing it.</p>
+          <p>Jax was eventually shutdown when ownership of the community was passed on, and the new owner had different plans for managing it - but I have plans to recreate Jax in a new upcoming project known as Gaffer.</p>
+
+          <div className={"text-title hide-print"}>This Site!</div>
+          <p className={"hide-print"}>This site was developed using Vite + React + Typescript!</p>
+          <p className={"hide-print"}>This is my first foray into the world of React, so depending on when you're seeing this it could be super early stage with barely anything... or something really fancy! Who knows!</p>
         </div>
       </div>
     </>
